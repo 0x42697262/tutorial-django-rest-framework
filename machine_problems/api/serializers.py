895c494a74e22ab1ab2b4fdb49d10b93cc23a0b8
KrulYuno <giogulane.wineffgulane@gmail.com>
2023-07-11 20:31:25 +0900

    made a working create for the two models. no update tho

--- machine_problems/api/serializers.py
+++ machine_problems/api/serializers.py
@@ -9,17 +9,33 @@ class StringInputsSerializer(serializers.ModelSerializer):
         fields  = ['string_input']
 
 class TestCasesSerializer(serializers.ModelSerializer):
-    # regex  = serializers.SlugRelatedField(many=True, read_only=True, slug_field='string_input')
-    # strings  = StringInputsSerializer(many=True)
+    strings  = StringInputsSerializer(many=True)
     class Meta:
         model   = TestCases
-        fields  = ['id', 'regex']
-        # fields  = ['id', 'regex', 'strings']
+        fields  = ['id', 'regex', 'strings']
 
-    # def create(self, validated_data):
-    #     strings_data    = validated_data.pop('string')
-    #     test_case       = TestCases.objects.create(**validated_data)
-    #     for string in strings_data:
-    #         StringInputs.objects.create(test_case=test_case, **string)
-    #     return test_case
-    #
+    def create(self, validated_data):
+        strings_data    = validated_data.pop('strings')
+        case            = TestCases.objects.create(**validated_data)
+        for string in strings_data:
+            StringInputs.objects.create(case=case, **string)
+        return case
+
+    def update(self, instance, validated_data):
+        strings_data    = validated_data.pop('strings')
+        instance.regex  = validated_data.get('regex', instance.regex)
+        instance.save()
+
+        existing_string_inputs      = instance.strings.all()
+        existing_string_inputs_ids  = [str(input.id) for input in existing_string_inputs]
+
+        for string_data in strings_data:
+            string_id = string_data.get('id', None)
+            if string_id in existing_string_inputs_ids:
+                string_input                = existing_string_inputs.get(id=string_id)
+                string_input.string_input   = string_data.get('string_input', string_input.string_input)
+                string_input.save()
+            else:
+                StringInputs.objects.create(case=instance, **string_data)
+
+        return instance
