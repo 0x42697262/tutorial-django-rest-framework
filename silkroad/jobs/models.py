6a8be4294f350ac202ad321263cf44f256ec7d97
KrulYuno <giogulane.wineffgulane@gmail.com>
2023-07-15 12:23:08 +0900

    changed to TextField for title and desc from CharField

--- silkroad/jobs/models.py
+++ silkroad/jobs/models.py
@@ -17,8 +17,8 @@ class Job(models.Model):
     """
     owner           = models.ForeignKey(User, on_delete=models.CASCADE)
     status          = models.IntegerField(choices=STATUS_CHOICES, default=0)
-    title           = models.CharField(max_length=32, null=True)
-    description     = models.CharField(max_length=256, null=True)
+    title           = models.TextField(max_length=32, null=True)
+    description     = models.TextField(max_length=256, null=True)
 
     created_at      = models.DateTimeField(auto_now_add=True)
     modified_at     = models.DateTimeField(auto_now=True)
