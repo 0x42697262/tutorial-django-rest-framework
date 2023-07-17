d72b277043a1b8ce59e8af1883bc3bc5023c46b8
KrulYuno <giogulane.wineffgulane@gmail.com>
2023-07-17 13:38:04 +0900

    added default False to is_deleted

--- silkroad/jobs/models.py
+++ silkroad/jobs/models.py
@@ -22,7 +22,7 @@ class Job(models.Model):
 
     created_at      = models.DateTimeField(auto_now_add=True)
     modified_at     = models.DateTimeField(auto_now=True)
-    is_deleted      = models.BooleanField()
+    is_deleted      = models.BooleanField(default=False)
 
 
 
