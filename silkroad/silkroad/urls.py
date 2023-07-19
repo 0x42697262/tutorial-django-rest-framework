a2d5c118b585641a82aa975f4c5119b283aaeb8a
chickenny <wineff@automatonsystems.io>
2023-07-19 22:54:51 +0900

    update basename of applications

--- silkroad/silkroad/urls.py
+++ silkroad/silkroad/urls.py
@@ -1,9 +1,10 @@
 from django.contrib         import admin
 from django.urls            import path, include
+
 from rest_framework.routers import DefaultRouter
 
 from jobs.views             import JobViewSet
-from users.views            import UserViewSet 
+from users.views            import UserViewSet
 from applications.views     import ApplicationViewSet
 
 
@@ -18,10 +19,23 @@ router.register(
         UserViewSet,
         basename='user'
         )
-# router.register(r'applications', ApplicationViewSet, basename='application')
+router.register(
+        r'jobs/(?P<job_id>\d+)/applications',
+        ApplicationViewSet,
+        basename='application'
+        )
 
 urlpatterns = [
-    path('admin/',  admin.site.urls),
-    path('v1/',     include(router.urls)),
-    path('api-auth/', include('rest_framework.urls')),
+    path(
+        'admin/',
+        admin.site.urls
+        ),
+    path(
+        'v1/',
+        include(router.urls)
+        ),
+    path(
+        'api-auth/',
+        include('rest_framework.urls')
+        ),
 ]
